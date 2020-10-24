d1e8feb29732c51cd61de6e4f2fe2fc1a48f55ea
annaswitaj <annaswitaj.sw@gmail.com>
2020-10-24 13:46:14 +0200

    change name folder

---
+++ Projekt/Assets/Scripts/JumpScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    public float force = 5.0f;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+         Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(0, 0, force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}

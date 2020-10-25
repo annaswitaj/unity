8fbfc44cfc2f924cde211b3c723c82783e776741
annaswitaj <annaswitaj.sw@gmail.com>
2020-10-25 17:36:09 +0100

    add tasks

---
+++ Zadania/Assets/Scripts/RightLeft.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RightLeft : MonoBehaviour
+{
+    private int direction = -1;
+    public float speed = 0.5f;
+    void Start()
+    {
+        transform.position = new Vector3(1f, 0.0f, 0.0f);
+       
+    }
+    void Update()
+    {
+        if (transform.position.x>=11f || transform.position.x <= 1f)
+        {
+            direction *= -1;
+        }
+      
+        transform.Translate(speed*direction, 0f, 0f);
+    }
+}
+
---
+++ Zadania/Assets/Scripts/Squaremove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squaremove : MonoBehaviour
+{
+    public float speed = 0.5f;
+   
+
+    void Start()
+    {
+        transform.position = new Vector3(0f, 0f, 1f);
+
+    }
+    void Update()
+    {
+        // if ((transform.position.x == 0f && transform.position.z == 1f) || (transform.position.x == 10f && transform.position.z == 1f) || (transform.position.x == 10f && transform.position.z == 10f) || (transform.position.x == 0f && transform.position.z== 10f))
+        //{
+        //   transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+        //}
+        // transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+
+        if ((transform.position.x == 0f && transform.position.z < 10f))
+            {
+            transform.Translate(0f, 0f, speed);
+            if(transform.position.z>10f)
+            {
+                transform.position= new Vector3(0f, 0f, 10f);
+            }
+        }
+
+        if((transform.position.z == 10f && transform.position.x < 9f))
+            {
+            transform.Translate(speed, 0f, 0f);
+            if (transform.position.x > 9f)
+            {
+                transform.position = new Vector3(9f, 0f, 10f);
+            }
+        }
+
+        if ((transform.position.x == 9f && transform.position.z> 0f))
+        {
+            transform.Translate(0f, 0f, -speed);
+            if (transform.position.z < 1f)
+            {
+                transform.position = new Vector3(9f, 0f, 1f);
+            }
+        }
+
+        if ((transform.position.x > 0f && transform.position.z == 1f))
+        {
+            transform.Translate(-speed, 0f, 0f);
+            if (transform.position.x < 0f)
+            {
+                transform.position = new Vector3(0f, 0f, 1f);
+            }
+        }
+
+
+    }
+}
